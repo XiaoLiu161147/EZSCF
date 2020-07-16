7807a21b31ec453f9ebe54accb8b0ce1702c7660
Xiao Liu <49647370+XiaoLiu161147@users.noreply.github.com>
2020-07-16 23:28:49 +0800

    Update EZSCF.py

--- EZSCF.py
+++ EZSCF.py
@@ -1,13 +1,12 @@
 '''
 EZSCF is the name of this purely Python-based RHF/RMP2 program of atomic systems, mainly used for the bachelor's thesis of Xiao Liu @ BNU.
-The name EZSCF is a homophone with "Easy SCF" and also a pun on the initial Chinese Pinyin letters of Xiao's beloved Alma Mater, Hangzhou No.2 High School (aka 杭州二中 or HZEZ).
+The name EZSCF is a homophone with "Easy SCF" and also a pun on the initial Chinese Pinyin letters of my beloved Alma Mater, Hangzhou No.2 High School (aka 杭州二中 or HZEZ).
 Differing from widely used Cartesian GTO basis set, this program applies Spherical Harmonic GTO basis set so as to compute high angular momentum part more efficiently and accurately.
 
 Developed by Xiao Liu under the guidance of Prof. Zhendong Li @ BNU. All references are marked in different places of the program.
 Many thanks to Rui Li (from ZJU to Caltech; HZEZ alumnus), Jingze Li (from BNU to UCSD), Jie Feng (from HUST to SJTU; HZEZ alumnus) & Ruiyi Zhou (from HKU to UNC Chapel Hill) for helpful discussions!
 A special thank goes to Sobereva & Warm_Cloud.
 Last but the most important, thanks Qiming Sun (also HZEZ alumnus) for his prominent open-source program PySCF!
-
 Feel free to contact Xiao Liu via email at the address of 201611150142@mail.bnu.edu.cn if you have any questions or advice.
 
 Xiao Liu
@@ -242,7 +241,7 @@ def form_unnormed_S_matrix(tuple_list):
     return S_matrix
 
 def get_S_norm_factor(n):
-    # Normalization form the value of diagonal elements to get the same output with Gaussian and PySCF
+    # Normalize diagonal elements of S matrix to be unitary, for the sake of getting the same output with Gaussian and PySCF
     global unnormed_S_matrix
     return 1/msqrt(unnormed_S_matrix[n][n])
 
