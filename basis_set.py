d2878209baac3ae2287de74422197832ba784395
Xiao Liu <49647370+XiaoLiu161147@users.noreply.github.com>
2020-08-31 23:51:57 +0800

    Update basis_set.py

--- basis_set.py
+++ basis_set.py
@@ -18,6 +18,7 @@ import re
 import numpy as np
 
 periodic_table = [
+'charge',
 'H', 'He',
 'Li', 'Be', 'B', 'C', 'N', 'O', 'F', 'Ne',
 'Na', 'Mg', 'Al', 'Si', 'P', 'S', 'Cl', 'Ar',
@@ -28,7 +29,7 @@ periodic_table = [
 ]
 
 def atomic_num(atom):
-    return periodic_table.index(atom)+1
+    return periodic_table.index(atom)
 
 def get_l(orbital_name):
     orbital_type = re.sub('[^A-Z]', '', orbital_name)
@@ -41,11 +42,7 @@ def get_l(orbital_name):
     if orbital_type == 'H':return 5
     if orbital_type == 'I':return 6
 
-# for Dunning basis set only
 class basis_set:
-
-    cc_pVnZ = ['cc-pVDZ', 'cc-pVTZ', 'cc-pVQZ', 'cc-pV5Z', 'cc-pV6Z']
-    aug_cc_pVnZ = ['aug-cc-pVDZ', 'aug-cc-pVTZ', 'aug-cc-pVQZ', 'aug-cc-pV5Z', 'aug-cc-pV6Z']
 
     def __init__(self, atom, basis_set_name):
         self.atom = atom
@@ -62,6 +59,7 @@ class basis_set:
         if self.basis_set_name[-2] == '6': return 6
 
     def layer_num(self):
+        if self.atom in ['charge']: return 0
         if self.atom in ['H', 'He']: return 1
         if self.atom in ['Li', 'Be', 'B', 'C', 'N', 'O', 'F', 'Ne']: return 2
         if self.atom in ['Na', 'Mg', 'Al', 'Si', 'P', 'S', 'Cl', 'Ar']: return 3
@@ -71,41 +69,47 @@ class basis_set:
         if self.atom in ['Fr', 'Ra', 'Ac', 'Th', 'Pa', 'U', 'Np', 'Pu', 'Am', 'Cm', 'Bk', 'Cf', 'Es', 'Fm','Md', 'No', 'Lr','Rf', 'Db', 'Sg', 'Bh', 'Hs', 'Mt', 'Ds', 'Rg', 'Cn', 'Nh', 'Fl', 'Mc', 'Lv', 'Ts', 'Og']: return 7
 
     def wfn(self):
-        text = open(self.atom_bs() + '.txt')
-        wfn = {}
+        if self.atom != 'charge' and 'MM' not in self.atom:
+            text = open(self.atom_bs() + '.txt')
+            wfn = {}
 
-        gto_num = 0
-        sto_num = 0
-        temp_gto_num_1 = 0
-        temp_gto_num_2 = 0
-        line_num = 0
+            gto_num = 0
+            sto_num = 0
+            temp_gto_num_1 = 0
+            temp_gto_num_2 = 0
+            line_num = 0
 
-        for line in text:
-            line_split = line.split()
-            line_num += 1
+            for line in text:
+                line_split = line.split()
+                line_num += 1
 
-            if line_num == 1:
-                continue
+                if line_num == 1:
+                    continue
 
-            elif line_split[0].isalpha() == True:
-                sto_type = line_split[0]
-                alpha_list = []
-                concn_list = []
-                gto_num = int(line_split[1])
-                sto_num = int(line_split[2])
-                temp_gto_num_1 = 0
-                temp_gto_num_2 = 0
+                elif line_split[0].isalpha() == True:
+                    sto_type = line_split[0]
+                    alpha_list = []
+                    concn_list = []
+                    gto_num = int(line_split[1])
+                    sto_num = int(line_split[2])
+                    temp_gto_num_1 = 0
+                    temp_gto_num_2 = 0
 
-            elif temp_gto_num_1 != gto_num:
-                temp_gto_num_1 += 1
-                alpha_list.append(float(line_split[0].replace('D', 'E')))
-                alpha_array = np.array([alpha_list]).T
-            elif temp_gto_num_1 == gto_num:
-                temp_gto_num_2 += 1
-                for i in range(sto_num):
-                    concn_list.append(float(line_split[i].replace('D', 'E')))
-            if temp_gto_num_2 == gto_num:
-                concn_array = np.array(concn_list).reshape(gto_num, sto_num)
-                wfn[sto_type] = alpha_array, concn_array
+                elif temp_gto_num_1 != gto_num:
+                    temp_gto_num_1 += 1
+                    alpha_list.append(float(line_split[0].replace('D', 'E')))
+                    alpha_array = np.array([alpha_list]).T
+                elif temp_gto_num_1 == gto_num:
+                    temp_gto_num_2 += 1
+                    for i in range(sto_num):
+                        concn_list.append(float(line_split[i].replace('D', 'E')))
+                if temp_gto_num_2 == gto_num:
+                    concn_array = np.array(concn_list).reshape(gto_num, sto_num)
+                    wfn[sto_type] = alpha_array, concn_array
 
-        return wfn
+            return wfn
+
+        else:
+            wfn = {'S': (np.zeros((1,1)), np.zeros((1,1)))}
+
+            return wfn
