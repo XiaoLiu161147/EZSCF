87ca397d3da240d23ebc0edbdc83a648f310d36e
Xiao Liu <49647370+XiaoLiu161147@users.noreply.github.com>
2020-11-01 23:53:11 +0800

    Update EZSCF.py

--- EZSCF.py
+++ EZSCF.py
@@ -1,12 +1,14 @@
 '''
+# EZSCF
 EZSCF is the name of this purely Python-based RHF/RMP2 program of atomic systems, mainly used for the bachelor's thesis of Xiao Liu @ BNU.
-The name EZSCF is a homophone with "Easy SCF" and also a pun on the initial Chinese Pinyin letters of my beloved Alma Mater, Hangzhou No.2 High School (aka 杭州二中 or HZEZ).
-Differing from widely used Cartesian GTO basis set, this program applies Spherical Harmonic GTO basis set so as to compute high angular momentum part more efficiently and accurately.
+The name EZSCF is a homophone with "Easy SCF" and also a pun on the initial Chinese Pinyin letters of Xiao's beloved Alma Mater, Hangzhou No.2 High School (aka 杭州二中 or HZEZ).
+Differing from widely used Cartesian GTO basis set, this program applies Spherical Harmonic GTO basis set so as to evaluate high angular momentum terms of the electron integrals more efficiently and accurately in a recursion-free way.
 
-Developed by Xiao Liu under the guidance of Prof. Zhendong Li @ BNU. All references are marked in different places of the program.
+EZSCF is developed by Xiao Liu under the guidance of Prof. Zhendong Li @ BNU. All references are marked in different places of the program.
 Many thanks to Rui Li (from ZJU to Caltech; HZEZ alumnus), Jingze Li (from BNU to UCSD), Jie Feng (from HUST to SJTU; HZEZ alumnus) & Ruiyi Zhou (from HKU to UNC Chapel Hill) for helpful discussions!
 A special thank goes to Sobereva & Warm_Cloud.
 Last but the most important, thanks Qiming Sun (also HZEZ alumnus) for his prominent open-source program PySCF!
+
 Feel free to contact Xiao Liu via email at the address of 201611150142@mail.bnu.edu.cn if you have any questions or advice.
 
 Xiao Liu
